230ac7fd4b5328fa69f6ec3b42dfcd88221ea908
Joshua Jeschek <joshua@jeschek.eu>
2023-04-14 10:08:36 +0200

    feat: initial commit

---
+++ apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ArtistApi';
+export * from './ArtworkApi';
+export * from './ExhibitionApi';
+export * from './UploadFileApi';
+export * from './UsersPermissionsAuthApi';
+export * from './UsersPermissionsUsersRolesApi';
