c0e6823108faee781f718d2b709f8faf75ed9a64
Joshua Jeschek <joshua@jeschek.eu>
2023-04-15 10:53:15 +0000

    feat: zwischen-welten/cms@1c395f6f8da188b0e0457e6de2cca3f8a08429d8
    feat: index single type

--- apis/index.ts
+++ apis/index.ts
@@ -3,6 +3,7 @@
 export * from './ArtistApi';
 export * from './ArtworkApi';
 export * from './ExhibitionApi';
+export * from './IndexApi';
 export * from './UploadFileApi';
 export * from './UsersPermissionsAuthApi';
 export * from './UsersPermissionsUsersRolesApi';
